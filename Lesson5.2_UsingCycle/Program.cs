7d0d810b85238a8a79656b993730ebb43ac60425
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-19 15:48:34 +0300

    add Cycle within cycle within a Lesson5.2

---
+++ Lesson5.2_UsingCycle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number from the keyboard: ");
+
+            int n = int.Parse(Console.ReadLine()!);
+            Console.Write("\n"); // символ переноса на новую строку
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.Write("\n"); // символ переноса на новую строку
+            }
+        }
+    }
+}
