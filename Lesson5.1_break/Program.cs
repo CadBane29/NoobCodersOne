2abe7f754c50cad69730bf57cf0a714f4d6b7bbd
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-19 15:22:55 +0300

    add Lesson5.1 Operator break|continue

---
+++ Lesson5.1_break/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number from the keyboard");
+
+            int n = int.Parse(Console.ReadLine()!);
+            //int counter = 0;
+
+            // while (true)
+            // {
+            //     Console.WriteLine($"Meaning counter = {counter}");
+            //     counter++;
+            //     if (counter > n)
+            //     {
+            //         break;
+            //     }
+            // }
+            // Console.WriteLine("Arghhh...");
+            for (int i = 0;i < n; i++)
+            {
+                if(i % 2 != 0)
+                {    
+                    continue;
+                }
+                Console.WriteLine($"Meaning value = {i}");
+            }
+        }
+    }
+
+}
