20c44d523ea0945f59bb0c9ba9d76e1c5af01076
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-19 15:02:31 +0300

    add Three Various Cycle to Lesson5

---
+++ Lesson5_Cycle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number from the keyboard");
+
+            int n = int.Parse(Console.ReadLine()!);
+            int counter = 0;
+            // for (int i = 0; i <= n; i++)
+            // {
+            //     Console.WriteLine($"Number = {i}");
+            // }
+            // while (counter < n)
+            // {
+            //     Console.WriteLine($"Number = {counter}");
+            //     counter++;
+            // }
+            do   // В любом случае делает одну итерацию
+            {
+                Console.WriteLine($"Number = {counter}");
+                counter++;
+            }
+            while (counter < n);
+        }
+    }
+
+}
