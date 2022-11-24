1c6012d4612b82e4013c92b93ee00e432f234ee8
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-24 14:04:59 +0300

    add Task000 Method Square

---
+++ Task000/Program.cs
@@ -0,0 +1,32 @@
+﻿// 0. Демонстрация решения
+// Напишите программу, которая 
+// 1. на вход принимает число и 
+// 2. выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9
+// -7 -> 49
+int num = 0;
+
+int Input(int num)
+{
+    num = -1;
+    while (num <= 0)
+    {
+        Console.Write("Enter number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num <= 0) num = -num;
+    }
+    return num;
+}
+
+int Output(int number)
+{
+    int result = number * number;
+    return result;
+}
+
+int input = Input(num);
+int output = Output(input);
+Console.WriteLine(output);
